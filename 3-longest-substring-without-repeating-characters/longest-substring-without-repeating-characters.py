54bf4f8f08bf08984a1c0126cc65e10c08a9fa95
Hardik Nahata <hardiknahata@gmail.com>
2024-10-23 00:30:56 -0700

    Time: 36 ms (94.69%) | Memory: 16.9 MB (17.15%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,7 +4,7 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         counts = Counter()
         ans = 0
-        
+
         # Initialize two pointers for the sliding window (left and right)
         left = right = 0
 
@@ -12,7 +12,6 @@ class Solution:
         while right < len(s):
             # Get the current character at the right pointer
             current_char = s[right]
-
             # Add or update the count of this character in the window
             counts[current_char] += 1
 
@@ -21,16 +20,13 @@ class Solution:
             while counts[current_char] > 1:
                 # Get the character at the left pointer
                 left_char = s[left]
-                
                 # Decrease the count of the left character as we're moving left pointer to the right
                 counts[left_char] -= 1
-
                 # Move the left pointer to shrink the window
                 left += 1
 
             # Calculate the current window size (right - left + 1) and update the max length (ans)
             ans = max(ans, right - left + 1)
-
             # Move the right pointer to expand the window
             right += 1
 
