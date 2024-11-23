f70db7a395a12d7561a9d6cff3f80b65676534a4
Hardik Nahata <hardiknahata@gmail.com>
2024-11-22 18:37:19 -0800

    Time: 21 ms (24.14%) | Memory: 21.7 MB (20.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = defaultdict(list)
+        
         for word in strs:
-            # Use a tuple of character counts as the key
-            count = [0] * 26  # Assuming only lowercase English letters
-            for char in word:
-                count[ord(char) - ord('a')] += 1
+            count = [0]*26
+            for ch in word:
+                count[ord(ch)-ord('a')]+=1
             anagrams[tuple(count)].append(word)
         
-        # Collect and return the grouped anagrams
         return list(anagrams.values())
-
-# TC = O(n * k), where n is the number of words and k is the average word length
-# SC = O(n * k), for storing the grouped anagrams and the frequency count keys
