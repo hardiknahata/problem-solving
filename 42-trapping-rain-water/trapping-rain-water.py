8f823e7475f50577f636bc7a98d6b18913c9a815
Hardik Nahata <hardiknahata@gmail.com>
2024-11-22 16:24:15 -0800

    Time: 10 ms (77.98%) | Memory: 18.3 MB (91.55%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,21 +1,20 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        N = len(height)
-        left, right = 0, N-1
-        l_max,r_max = 0, 0
-        ans = 0
+        res = 0  # Total water trapped
+        l, r = 0, len(height) - 1  # Two pointers
+        lmax, rmax = height[l], height[r]  # Track max heights from left and right
 
-        while left < right:
-            if height[left] < height[right]:
-                l_max = max(l_max, height[left])
-                ans += (l_max - height[left])
-                left += 1
-            else:
-                r_max = max(r_max, height[right])
-                ans += (r_max - height[right])
-                right -= 1
-        
-        return ans
+        while l < r:
+            if lmax < rmax:  # Process left side
+                l += 1
+                lmax = max(lmax, height[l])  # Update left max height
+                res += lmax - height[l]  # Add trapped water
+            else:  # Process right side
+                r -= 1
+                rmax = max(rmax, height[r])  # Update right max height
+                res += rmax - height[r]  # Add trapped water
 
-# TC = O(N)
-# SC = O(1)
+        return res  # Return the total trapped water
+
+# TC = O(n), single pass through the array using two pointers
+# SC = O(1), constant space used
