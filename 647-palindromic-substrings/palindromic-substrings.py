35fc8cb67bf3515edd3932241c074330d7a3c7ea
Hardik Nahata <hardiknahata@gmail.com>
2024-11-26 15:13:49 -0800

    Time: 128 ms (41.25%) | Memory: 17.3 MB (24.46%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -2,15 +2,20 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         def checkPalindromeFromCenter(s, i, j):
             counts = 0
+            # Expand outward from the center and count palindromic substrings
             while i >= 0 and j < len(s) and s[i] == s[j]:
-                counts +=1
+                counts += 1
                 i -= 1
                 j += 1
             return counts
         
         ans = 0
         for i in range(len(s)):
-            ans += checkPalindromeFromCenter(s, i, i) # odd
-            ans += checkPalindromeFromCenter(s, i, i+1) # even
+            ans += checkPalindromeFromCenter(s, i, i)  # Count odd-length palindromes
+            ans += checkPalindromeFromCenter(s, i, i + 1)  # Count even-length palindromes
             
-        return ans
+        return ans  # Return total count of palindromic substrings
+
+# TC = O(n^2), where n is the length of the string
+# - Outer loop runs O(n), and inner palindrome expansion takes O(n) in the worst case.
+# SC = O(1), constant space is used
