fc5028d039efd89b999aeb92ed597db5ba5cc0b5
Hardik Nahata <hardiknahata@gmail.com>
2024-12-01 13:15:47 -0800

    Time: 61 ms (40.48%) | Memory: 17.8 MB (9.04%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,22 +1,20 @@
-def check_palindrome(s,i,j):
-        while i<j:
-            if s[i] != s[j]:
-                return False
-            i += 1
-            j -= 1
-        return True
-
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        i, j = 0, len(s)-1
-
-        while i<j:
+        def checkPalindrome(s, i, j):
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+        
+        i = 0
+        j = len(s)-1
+        while i < j:
             if s[i] != s[j]:
-                return check_palindrome(s, i, j - 1) or check_palindrome(s, i + 1, j)
+                return checkPalindrome(s, i+1, j) or checkPalindrome(s, i, j-1)
             i += 1
             j -= 1
         return True
 
-        
-
-        
+            
